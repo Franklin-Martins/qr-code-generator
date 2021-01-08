84c20e890aa4342864fd2c95d609b804bad62e3a
Franklin <gentilezaxd@gmail.com>
2021-01-08 12:48:42 -0400

    Create function to create user badge and add generatedImage.html

--- src/index.js
+++ src/index.js
@@ -6,3 +6,22 @@ async function clearAllFileds() {
     document.getElementById("twitter").value = "";
     document.getElementById("github").value = "";
 }
+
+function createUserBadge(){
+    try {
+        const name = document.forms["myForm"]["name"].value;
+        const email = document.forms["myForm"]["email"].value;
+        const twitter = document.forms["myForm"]["twitter"].value;
+        const github = document.forms["myForm"]["github"].value;
+        if(name == "" && email){
+            alert("Informe nome e email válidos")
+            return false
+        }
+        sessionStorage.setItem("name", name);
+        sessionStorage.setItem("email", email);
+        sessionStorage.setItem("name", twitter);
+        sessionStorage.setItem("email", github);
+    } catch (error) {
+        return console.log(error)
+    }
+}
