a501aeccda814bdd3e5ffe5ead1a1d6fb13f3c29
Franklin <gentilezaxd@gmail.com>
2021-01-26 21:33:46 -0400

    Fix form submission

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const routes = require('./routes')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 
-app.use('/', routes)
 app.use('/static', express.static(__dirname + '/public'));
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use('/', routes)
 
 app.listen(3030)
 console.log('development server started on port 3030')
